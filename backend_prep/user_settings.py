4d9a33dfdcf0221c0913a5a43d8fd41dfd870c90
michael shintani <mikeshintani@yahoo.com>
2022-05-04 19:11:16 -0700

    uploaded django, djangorestframework, mysqlclient thru venv + created backend_api proj + created backend_prep_api app + created models, serializer, urls py files in app + moved sensi settings to user_settings in proj

---
+++ backend_prep/user_settings.py
@@ -0,0 +1,16 @@
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-j)0s2guju+b22h+2h2q^opqu_rwv@jsg$5(!e@+5u0(2!vtd_n'
+
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'djangorestframework',
+    'mysqlcient',
+
+]
---
+++ backend_prep_api/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class BackendPrepApiConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'backend_prep_api'
